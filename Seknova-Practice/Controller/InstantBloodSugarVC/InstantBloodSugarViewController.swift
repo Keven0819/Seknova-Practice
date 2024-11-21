edb03afe88057761b1aaa2ceb4f6185489ade155
Keven <s5210apple@gmail.com>
2024-11-21 18:14:44 +0800

    啟用感測器頁面 輸入成功並提示驗證成功且跳到下一頁 完成

---
+++ Seknova-Practice/Controller/InstantBloodSugarVC/InstantBloodSugarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InstantBloodSugarViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-2627 on 2024/11/21.
+//
+
+import UIKit
+
+class InstantBloodSugarViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - UI Settings
+    
+    // MARK: - IBAction
+    
+    // MARK: - Function
+    
+}
+
+// MARK: - Extensions
--- Seknova-Practice/Controller/ScanningSensorVC/ScanningSensorViewController.swift
+++ Seknova-Practice/Controller/ScanningSensorVC/ScanningSensorViewController.swift
@@ -26,9 +26,50 @@ class ScanningSensorViewController: UIViewController {
         self.navigationItem.hidesBackButton = true
     }
     // MARK: - IBAction
+    @IBAction func btnTextInputTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "文字輸入", message: "請輸入裝置 ID", preferredStyle: .alert)
+
+        alertController.addTextField { (textField) in
+            textField.placeholder = "輸入裝置 ID 後六碼"
+        }
+
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        let confirmAction = UIAlertAction(title: "確認", style: .default) { [weak self] _ in
+            guard let deviceID = alertController.textFields?.first?.text, deviceID.count == 6 else {
+                self?.showInvalidIDAlert()
+                return
+            }
+            
+            // 顯示驗證成功訊息
+            self?.showSuccessAlert()
+        }
+
+        alertController.addAction(cancelAction)
+        alertController.addAction(confirmAction)
+        present(alertController, animated: true, completion: nil)
+    }
     
     // MARK: - Function
+    private func showSuccessAlert() {
+        let successAlert = UIAlertController(title: "驗證成功", message: "裝置 ID 驗證成功", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "確定", style: .default) { [weak self] _ in
+            self?.navigateToMainPage()
+        }
+        successAlert.addAction(okAction)
+        present(successAlert, animated: true, completion: nil)
+    }
+
+    private func showInvalidIDAlert() {
+        let invalidAlert = UIAlertController(title: "無效的 ID", message: "請輸入正確的裝置 ID", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "確定", style: .default, handler: nil)
+        invalidAlert.addAction(okAction)
+        present(invalidAlert, animated: true, completion: nil)
+    }
     
+    private func navigateToMainPage() {
+        let instantBloodSugarVC = InstantBloodSugarViewController()
+        navigationController?.pushViewController(instantBloodSugarVC, animated: true)
+    }
 }
 
 // MARK: - Extensions
