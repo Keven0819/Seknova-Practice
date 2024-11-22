d291d6bb69b90eb5723d6caba67bd28219cb13e7
Keven <s5210apple@gmail.com>
2024-11-22 15:14:37 +0800

    綁定手機碼頁面 刻畫面完成

---
+++ Seknova-Practice/Controller/BindPhoneCallVC/BindPhoneCallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BindPhoneCallViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-2627 on 2024/11/22.
+//
+
+import UIKit
+
+class BindPhoneCallViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - UI Settings
+    
+    // MARK: - IBAction
+    
+    // MARK: - Function
+    
+}
+
+// MARK: - Extensions
