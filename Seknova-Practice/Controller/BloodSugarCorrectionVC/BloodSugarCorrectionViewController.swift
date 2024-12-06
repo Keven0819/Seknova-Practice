7c05d372b893467e4e3ed26896a1918191d087ef
Keven <s5210apple@gmail.com>
2024-12-06 17:13:22 +0800

    血糖校正畫面 儲存按鈕完成

--- Seknova-Practice/Controller/BloodSugarCorrectionVC/BloodSugarCorrectionViewController.swift
+++ Seknova-Practice/Controller/BloodSugarCorrectionVC/BloodSugarCorrectionViewController.swift
@@ -11,20 +11,29 @@ class BloodSugarCorrectionViewController: UIViewController , BloodSugarCorrectio
     
     // MARK: - IBOutlet
     @IBOutlet weak var btnKnowMore: UIButton!
+    @IBOutlet weak var lbBloodSugarValue: UILabel!
+    @IBOutlet weak var btnSetup: UIButton!
+    @IBOutlet weak var btnSetdown: UIButton!
     
     // MARK: - Property
+    
+    var bloodSugarNum = [Int](0...400)
+    var timer: Timer?
     
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        addLongPressGestures()
     }
     
     // MARK: - UI Settings
     
     func setupUI() {
         setupNavigationBar()
+        let bloodSuagrValue = UserPreferences.shared.bloodsugarCorrectionValue
+        lbBloodSugarValue.text = bloodSuagrValue
     }
     
     func setupNavigationBar() {
@@ -53,13 +62,91 @@ class BloodSugarCorrectionViewController: UIViewController , BloodSugarCorrectio
     }
     
     @IBAction func btnSaveTapped(_ sender: Any) {
-        let bloodSugarCorrectionSureVC = BloodSugarCorrectionSureViewController()
-        self.navigationController?.pushViewController(bloodSugarCorrectionSureVC, animated: true)
+        
+        // 如果低於55或400以上，跳出警告視窗
+        // 抓到目前的lbBloodSugarValue.text
+        let currentBloodSugarValue = lbBloodSugarValue.text
+        // 判斷他在bloddSugarNum的位置
+        let index = bloodSugarNum.firstIndex(of: Int(currentBloodSugarValue!)!)
+        
+        if bloodSugarNum[index!] < 55 || bloodSugarNum[index!] > 400 {
+            let alert = UIAlertController(title: "請輸入正確的指數", message: "輸入的指數必須在55~400之間", preferredStyle: .alert)
+            let action = UIAlertAction(title: "確定", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        } else {
+            // 儲存進UserPreferences
+            UserPreferences.shared.bloodsugarCorrectionValue = lbBloodSugarValue.text!
+            print(UserPreferences.shared.bloodsugarCorrectionValue!)
+            let bloodSugarCorrectionSureVC = BloodSugarCorrectionSureViewController()
+            self.navigationController?.pushViewController(bloodSugarCorrectionSureVC, animated: true)
+        }
     }
     
+    @IBAction func btnSetupTapped(_ sender: Any) {
+        // 抓到目前的lbBloodSugarValue.text
+        let currentBloodSugarValue = lbBloodSugarValue.text
+        // 判斷他在bloddSugarNum的位置
+        let index = bloodSugarNum.firstIndex(of: Int(currentBloodSugarValue!)!)
+        // 設定下一個值
+        lbBloodSugarValue.text = String(bloodSugarNum[index! + 1])
+    }
+    
+    @IBAction func btnSetdownTapped(_ sender: Any) {
+        // 抓到目前的lbBloodSugarValue.text
+        let currentBloodSugarValue = lbBloodSugarValue.text
+        // 判斷他在bloddSugarNum的位置
+        let index = bloodSugarNum.firstIndex(of: Int(currentBloodSugarValue!)!)
+        // 設定前一個值
+        lbBloodSugarValue.text = String(bloodSugarNum[index! - 1])
+    }
+    
+    @objc func handleLongPressSetup(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            incrementBloodSugarValue()
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(incrementBloodSugarValue), userInfo: nil, repeats: true)
+        } else if gesture.state == .ended {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    @objc func handleLongPressSetdown(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            decrementBloodSugarValue()
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(decrementBloodSugarValue), userInfo: nil, repeats: true)
+        } else if gesture.state == .ended {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    @objc func incrementBloodSugarValue() {
+        guard let currentBloodSugarValue = lbBloodSugarValue.text, let currentValue = Int(currentBloodSugarValue),
+              let index = bloodSugarNum.firstIndex(of: currentValue), index < bloodSugarNum.count - 1 else {
+            return
+        }
+        lbBloodSugarValue.text = String(bloodSugarNum[index + 1])
+    }
+    
+    @objc func decrementBloodSugarValue() {
+        guard let currentBloodSugarValue = lbBloodSugarValue.text, let currentValue = Int(currentBloodSugarValue),
+              let index = bloodSugarNum.firstIndex(of: currentValue), index > 0 else {
+            return
+        }
+        lbBloodSugarValue.text = String(bloodSugarNum[index - 1])
+    }
     // MARK: - Function
     func didKowMore() {
         print("didKowMore")
+    }
+    
+    func addLongPressGestures() {
+        let longPressSetup = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressSetup(_:)))
+        btnSetup.addGestureRecognizer(longPressSetup)
+
+        let longPressSetdown = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressSetdown(_:)))
+        btnSetdown.addGestureRecognizer(longPressSetdown)
     }
 }
 
