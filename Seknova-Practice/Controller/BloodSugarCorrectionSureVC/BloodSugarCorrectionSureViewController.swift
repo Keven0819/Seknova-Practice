8df1356c1fc254f3a6c4f0308cbe5f0819a25d63
Keven <s5210apple@gmail.com>
2024-12-11 17:45:12 +0800

    血糖校正確認畫面完成

--- Seknova-Practice/Controller/BloodSugarCorrectionSureVC/BloodSugarCorrectionSureViewController.swift
+++ Seknova-Practice/Controller/BloodSugarCorrectionSureVC/BloodSugarCorrectionSureViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class BloodSugarCorrectionSureViewController: UIViewController {
     
     // MARK: - IBOutlet
+    @IBOutlet weak var lbBloodSugarValue: UILabel!
+    @IBOutlet weak var lbTime: UILabel!
+    @IBOutlet weak var btnConfirm: UIButton!
+    @IBOutlet weak var btnCorrect: UIButton!
     
     // MARK: - Property
     
@@ -17,14 +21,44 @@ class BloodSugarCorrectionSureViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
     }
     
     // MARK: - UI Settings
-    
+    func setUI() {
+        loadingBloodSugarValue()
+        catchTime()
+    }
     // MARK: - IBAction
+    @IBAction func btnConfirmTapped(_ sender: Any) {
+        // 跳轉回即時血糖畫面MainviewController的即時血糖畫面
+        let mainVC = MainViewController()
+        self.navigationController?.pushViewController(mainVC, animated: true)
+    }
+    
+    @IBAction func btnCorrectTapped(_ sender: Any) {
+        // 回到血糖校正畫面
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     
     // MARK: - Function
+    func loadingBloodSugarValue() {
+        let value = UserPreferences.shared.bloodsugarCorrectionValue
+        lbBloodSugarValue.text = value
+    }
     
+    func catchTime() {
+        // 抓取目前的時間然後做成 2:14 上午的格式，讓label顯示
+        let date = Date()
+        let formatter = DateFormatter()
+        
+        // am pm 要中文顯示
+        formatter.locale = Locale(identifier: "zh_TW")
+        formatter.dateFormat = "h:mm a"
+        let time = formatter.string(from: date)
+        lbTime.text = time
+    }
 }
 
 // MARK: - Extensions
