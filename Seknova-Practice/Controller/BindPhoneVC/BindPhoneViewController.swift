b731436d1c9401a05fb9930e5e0ff3af4bdfeab6
Keven <s5210apple@gmail.com>
2025-01-13 15:30:14 +0800

    個人資訊頁面選單

---
+++ Seknova-Practice/Controller/BindPhoneVC/BindPhoneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BindPhoneViewController.swift
+//  Seknova-Practice
+//
+//  Created by imac-2627 on 2025/1/13.
+//
+
+import UIKit
+
+class BindPhoneViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - UI Settings
+    
+    // MARK: - IBAction
+    
+    // MARK: - Function
+    
+}
+
+// MARK: - Extensions
--- Seknova-Practice/Controller/PersonInformatoinVC/PersonInformationViewController.swift
+++ Seknova-Practice/Controller/PersonInformatoinVC/PersonInformationViewController.swift
@@ -12,6 +12,9 @@ class PersonInformationViewController: UIViewController {
     
     // MARK: - IBOutlet
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var dpkBirthday: UIDatePicker!
+    @IBOutlet weak var dpkToolBar: UIToolbar!
+    @IBOutlet weak var vDpkBackground: UIView!
     
     // MARK: - Property
     let userInformation = UserInformation()
@@ -91,206 +94,302 @@ extension PersonInformationViewController: UITableViewDelegate, UITableViewDataS
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: InfoTableViewCell.identifier, for: indexPath) as! InfoTableViewCell
         
-        // 利用email去篩選最新的資料
-        let realm = try! Realm()
-        let userInformation = realm.objects(UserInformation.self).filter("Email == %@", UserPreferences.shared.mail ?? "").first!
-        
-        cell.lbPhonenumber.isHidden = true
-        cell.imgvPhoneStatus.isHidden = true
-        cell.lbResult.isHidden = true
-        
         switch indexPath.section {
         case 0:
-            cell.btnLogOut.isHidden = true
             switch indexPath.row {
             case 0:
                 cell.lbName.text = "名"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.FirstName != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.FirstName
+                if userInformation.FirstName.isEmpty == false {
+                    cell.txfEdit.text = userInformation.FirstName
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 1:
                 cell.lbName.text = "姓"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.LastName != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.LastName
+                if userInformation.LastName.isEmpty == false {
+                    cell.txfEdit.text = userInformation.LastName
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 2:
                 cell.lbName.text = "出生日期"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Birthday != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
+                if userInformation.Birthday.isEmpty == false {
                     cell.lbResult.text = userInformation.Birthday
+                } else {
+                    cell.lbResult.text = ""
                 }
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 3:
                 cell.lbName.text = "電子信箱"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Email != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.Email
+                if userInformation.Email.isEmpty == false {
+                    cell.txfEdit.text = userInformation.Email
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 4:
                 cell.lbName.text = "手機號碼"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = false
-                cell.lbResult.isHidden = true
-                cell.lbPhonenumber.isHidden = true
-                if userInformation.Phone != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbPhonenumber.isHidden = false
-                    cell.lbPhonenumber.text = "+886" + userInformation.Phone
+                if userInformation.Phone.isEmpty == false {
+                    cell.lbPhonenumber.text = userInformation.Phone
+                } else {
+                    cell.lbPhonenumber.text = ""
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = false
+                cell.txfEdit.isHidden = true
+                cell.btnLogOut.isHidden = true
             case 5:
                 cell.lbName.text = "地址"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                if userInformation.Address != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.Address
+                if userInformation.Address.isEmpty == false {
+                    cell.txfEdit.text = userInformation.Address
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             default:
                 break
             }
         case 1:
-            cell.btnLogOut.isHidden = true
             switch indexPath.row {
             case 0:
                 cell.lbName.text = "性別"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Gender != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
+                if userInformation.Gender.isEmpty == false {
                     cell.lbResult.text = userInformation.Gender
+                } else {
+                    cell.lbResult.text = ""
                 }
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 1:
                 cell.lbName.text = "身高"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Height != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.Height
+                if userInformation.Height.isEmpty == false {
+                    cell.txfEdit.text = userInformation.Height
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 2:
                 cell.lbName.text = "體重"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Weight != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = userInformation.Weight
+                if userInformation.Weight.isEmpty == false {
+                    cell.txfEdit.text = userInformation.Weight
+                } else {
+                    cell.txfEdit.text = ""
+                    cell.txfEdit.placeholder = "點擊進行編輯"
                 }
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 3:
                 cell.lbName.text = "種族"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Race != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
+                if userInformation.Race.isEmpty == false {
                     cell.lbResult.text = userInformation.Race
+                } else {
+                    cell.lbResult.text = ""
                 }
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 4:
                 cell.lbName.text = "飲酒"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Liquor != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
+                if userInformation.Liquor.isEmpty == false {
                     cell.lbResult.text = userInformation.Liquor
+                } else {
+                    cell.lbResult.text = ""
                 }
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             case 5:
                 cell.lbName.text = "抽菸"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                if userInformation.Smoke != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
+                if userInformation.Smoke.isEmpty == false {
                     cell.lbResult.text = userInformation.Smoke
+                } else {
+                    cell.lbResult.text = ""
                 }
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
             default:
                 break
             }
         case 2:
-            cell.btnLogOut.isHidden = true
             switch indexPath.row {
             case 0:
                 cell.lbName.text = "發射器裝置"
-                cell.txfEdit.isHidden = false
-                cell.txfEdit.placeholder = "點擊進行編輯"
-                cell.imgvPhoneStatus.isHidden = true
-                cell.lbResult.isHidden = true
-                let transmitterID = UserPreferences.shared.transmitterDeviceID
-                if  transmitterID != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = transmitterID
+                if UserPreferences.shared.transmitterDeviceID?.isEmpty == false {
+                    cell.lbResult.text = UserPreferences.shared.transmitterDeviceID
+                } else {
+                    cell.lbResult.text = ""
                 }
-                
-                // 灰色箭頭
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
                 cell.accessoryType = .disclosureIndicator
             case 1:
                 cell.lbName.text = "感測器裝置"
-                let sensorID = UserPreferences.shared.sensorDeviceID
-                if  sensorID != "" {
-                    cell.txfEdit.isHidden = true
-                    cell.lbResult.isHidden = false
-                    cell.lbResult.text = sensorID
+                if UserPreferences.shared.sensorDeviceID?.isEmpty == false {
+                    cell.lbResult.text = UserPreferences.shared.sensorDeviceID
+                } else {
+                    cell.lbResult.text = ""
                 }
-                
-                // 灰色箭頭
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
                 cell.accessoryType = .disclosureIndicator
             case 2:
                 cell.lbName.text = "修改密碼"
-                
-                // 灰色箭頭
+                cell.txfEdit.isHidden = true
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
                 cell.accessoryType = .disclosureIndicator
             default:
                 break
             }
         case 3:
             cell.lbName.isHidden = true
+            cell.txfEdit.isHidden = true
+            cell.lbResult.isHidden = true
+            cell.imgvPhoneStatus.isHidden = true
             cell.btnLogOut.isHidden = false
+            cell.lbPhonenumber.isHidden = true
         default:
             break
         }
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 40
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: InfoTableViewCell.identifier, for: indexPath) as! InfoTableViewCell
+        
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 2:
+                // 出生日期
+                cell.lbName.text = "出生日期"
+                dpkToolBar.isHidden = false
+                dpkBirthday.isHidden = false
+                vDpkBackground.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
+            case 4:
+                cell.lbName.text = "手機號碼"
+                cell.lbName.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.imgvPhoneStatus.isHidden = false
+                cell.txfEdit.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = false
+                let bindVC = BindPhoneViewController()
+                navigationController?.pushViewController(bindVC, animated: true)
+            default:
+                break
+            }
+        case 1:
+            let alert = Alert()
+            switch indexPath.row {
+            case 0:
+                cell.lbName.text = "性別"
+                cell.lbName.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
+                let titles = ["生理男", "生理女", "其他"]
+                alert.showActionSheet(titles: titles, cancelTitle: "取消", vc: self) { result in
+                    cell.lbResult.isHidden = false
+                    cell.lbResult.text = result
+                }
+            case 3:
+                cell.lbName.text = "種族"
+                cell.lbName.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
+                let titles = ["亞洲", "非洲", "高加索", "拉丁", "其他"]
+                alert.showActionSheet(titles: titles, cancelTitle: "取消", vc: self) { result in
+                    cell.lbResult.isHidden = false
+                    cell.lbResult.text = result
+                }
+            case 4:
+                cell.lbName.text = "飲酒"
+                cell.lbName.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
+                let titles = ["不喝酒", "偶爾喝", "經常喝", "每天喝"]
+                alert.showActionSheet(titles: titles, cancelTitle: "取消", vc: self) { result in
+                    cell.lbResult.isHidden = false
+                    cell.lbResult.text = result
+                }
+            case 5:
+                cell.lbName.text = "抽菸"
+                cell.lbName.isHidden = false
+                cell.lbResult.isHidden = true
+                cell.txfEdit.isHidden = true
+                cell.imgvPhoneStatus.isHidden = true
+                cell.btnLogOut.isHidden = true
+                cell.lbPhonenumber.isHidden = true
+                let titles = ["有", "無"]
+                alert.showActionSheet(titles: titles, cancelTitle: "取消", vc: self) { result in
+                    cell.lbResult.isHidden = false
+                    cell.lbResult.text = result
+                }
+            default:
+                break
+            }
+            
+        default:
+            break
+        }
     }
 }
