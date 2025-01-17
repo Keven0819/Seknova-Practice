0097cc78df9f9e8d1b153e1c4506e19ae4d2b98a
Keven <s5210apple@gmail.com>
2025-01-17 13:53:39 +0800

    生活作息頁面的DatePicker基本功能完成(還未有儲存資料)

--- Seknova-Practice/Controller/LifeVC/LifeViewController.swift
+++ Seknova-Practice/Controller/LifeVC/LifeViewController.swift
@@ -10,20 +10,63 @@ import UIKit
 class LifeViewController: UIViewController {
     
     // MARK: - IBOutlet
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var vBackground: UIView!
+    @IBOutlet weak var lbDate: UILabel!
     
     // MARK: - Property
     
     // MARK: - LifeCycle
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        lbDate.text = currentDateTime()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePicker.maximumDate = Date()
     }
     
     // MARK: - UI Settings
     
     // MARK: - IBAction
+    @IBAction func btnDatePicker(_ sender: Any) {
+        datePicker.isHidden = false
+        toolBar.isHidden = false
+        vBackground.isHidden = false
+    }
+    
+    @IBAction func btnCnacel(_ sender: Any) {
+        datePicker.isHidden = true
+        toolBar.isHidden = true
+        vBackground.isHidden = true
+    }
+    
+    @IBAction func btnConfirm(_ sender: Any) {
+        datePicker.isHidden = true
+        toolBar.isHidden = true
+        vBackground.isHidden = true
+        print(dateFormate())
+        lbDate.text = dateFormate()
+    }
     
     // MARK: - Function
+    
+    func dateFormate() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_Hant_TW")
+        formatter.dateFormat = "yyyy/MM/dd EEEE aa hh:mm"
+        return formatter.string(from: datePicker.date)
+    }
+    
+    func currentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_Hant_TW")
+        formatter.dateFormat = "yyyy/MM/dd EEEE aa hh:mm"
+        return formatter.string(from: Date())
+    }
     
 }
 
